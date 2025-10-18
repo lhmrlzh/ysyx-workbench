e4aea0d83368134f7f392ee2a35120ae5988a528
rat <lhmrlzh@163.com>
2025-10-18 15:48:34 +0800

    ✨ feat: 更新相关配置，并完成E5部分内容
    1. 重新配置git_submit的姓名和ID
    2. 完成E5部分的波形测试

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "../build/Vtop.h"
+#include "verilated.h"
+#include "verilated_fst_c.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char** argv) {
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->traceEverOn(true);
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{ contextp };
+
+  VerilatedFstC* tfp = new VerilatedFstC;
+  top->trace(tfp, 99);
+  tfp->open("out/wave.fst");
+
+  int step = 100, i = 0;
+  while (!contextp->gotFinish() && i++ < step) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+
+  tfp->close();
+  delete tfp;
+  delete top;
+  delete contextp;
   return 0;
 }
