9d13c0e82ebbe4ee4460bd8ed0ac38a7a2284505
tracer-ysyx <tracer@ysyx.org>
2025-10-23 15:07:25 +0800

    > compile NEMU ysyx_100018154 刘志豪 Linux LEHOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:07:25 up  4:54,  1 user,  load average: 0.42, 0.18, 0.05

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -16,11 +16,11 @@
 #include "debug.h"
 #include <utils.h>
 
-NEMUState nemu_state = { .state = NEMU_STOP };
+NEMUState nemu_state = {.state = NEMU_STOP};
 
 int is_exit_status_bad() {
   Log("state: %d, halt_ret: %d\n", nemu_state.state, nemu_state.halt_ret);
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_QUIT);
+             (nemu_state.state == NEMU_STOP) || (nemu_state.state == NEMU_QUIT);
   return !good;
 }
