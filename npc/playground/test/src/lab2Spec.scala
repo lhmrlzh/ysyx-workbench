c368740be8dc68cc0a3686a1e0b9db274518f3d4
tracer-ysyx <tracer@ysyx.org>
2025-10-24 18:44:17 +0800

    > generate verilog ysyx_100018154 刘志豪 Linux rat 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:44:17 up  2:35,  1 user,  load average: 0.41, 0.30, 0.20

--- npc/playground/test/src/lab2Spec.scala
+++ npc/playground/test/src/lab2Spec.scala
@@ -6,7 +6,8 @@ import chisel3.simulator.EphemeralSimulator._
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 
-/** This is a trivial example of how to run this Specification From within sbt use:
+/** This is a trivial example of how to run this Specification From within sbt
+  * use:
   * {{{
   * testOnly gcd.GCDSpec
   * }}}
@@ -21,14 +22,31 @@ import org.scalatest.matchers.must.Matchers
   */
 class Lab2Spec extends AnyFreeSpec with Matchers {
   "Encoder8t3 should convert input into bits, with an enable bit" in {
-    simulate(new Encoder8t3(3)) { dut =>
-      val testValues = for {
-        x <- 0 to 10
-        y <- 0 to 10
-      } yield (x, y)
-      val inputSeq   = testValues.map { case (x, y) => (new GcdInputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U) }
-      val resultSeq  = testValues.map { case (x, y) =>
-        (new GcdOutputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U, _.gcd -> BigInt(x).gcd(BigInt(y)).U)
+    simulate(new Encoder(3)) { dut =>
+      val testValues = for { x <- 0 to 0xff } yield (x)
+      val inputSeq = testValues.map { x => (x.U, 1.B) }
+      val resultSeq = testValues.map { x =>
+        val bit_count = Integer.bitCount(x)
+        if (bit_count == 0) (0.U, 0.B)
+        else if (bit_count == 1) {
+          var count: Int = 0
+          var nx: Int = x
+          while (nx > 0) {
+            nx = nx >> 1
+            count = count + 1
+          }
+          (count.U, 1.B)
+        } else (0.U, 1.B)
+      }
+
+      for ((input, result) <- inputSeq.toSeq.zip(resultSeq.toSeq)) {
+        val (x, en) = input
+        val (y, in) = result
+
+        dut.io.x.poke(x)
+        dut.io.en.poke(en)
+        dut.io.y.expect(y)
+        dut.io.in.expect(in)
       }
 
       dut.reset.poke(true.B)
@@ -36,31 +54,6 @@ class Lab2Spec extends AnyFreeSpec with Matchers {
       dut.reset.poke(false.B)
       dut.clock.step()
 
-      var sent, received, cycles: Int = 0
-      while (sent != 100 && received != 100) {
-        assert(cycles <= 1000, "timeout reached")
-
-        if (sent < 100) {
-          dut.input.valid.poke(true.B)
-          dut.input.bits.value1.poke(testValues(sent)._1.U)
-          dut.input.bits.value2.poke(testValues(sent)._2.U)
-          if (dut.input.ready.peek().litToBoolean) {
-            sent += 1
-          }
-        }
-
-        if (received < 100) {
-          dut.output.ready.poke(true.B)
-          if (dut.output.valid.peekValue().asBigInt == 1) {
-            dut.output.bits.gcd.expect(BigInt(testValues(received)._1).gcd(testValues(received)._2))
-            received += 1
-          }
-        }
-
-        // Step the simulation forward.
-        dut.clock.step()
-        cycles += 1
-      }
     }
   }
 }
